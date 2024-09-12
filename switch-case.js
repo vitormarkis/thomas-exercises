679d7ac8fa7f41b15751e76d0600aceb1c58052f
Vitor Markis <vitormarkis2369@gmail.com>
2024-09-12 19:03:09 -0300

    criei switch case exercicio

---
+++ switch-case.js
@@ -0,0 +1,25 @@
+/**
+ * Quero contratar para minha empresa, um guia turístico.
+ * A pessoa chega pra ele, fala uma região do Brasil - Sul, Sudeste,
+ * Centro-Oeste ou Nordeste - e ele responde um estado dessa região que
+ * ele acha mais interessante (voce decide quais são).
+ */
+
+function conseguirMelhorEstadoDaRegiao(regiao) {}
+
+conseguirMelhorEstadoDaRegiao("Sul")
+conseguirMelhorEstadoDaRegiao("Sudeste")
+conseguirMelhorEstadoDaRegiao("Centro-Oeste")
+conseguirMelhorEstadoDaRegiao("Nordeste")
+
+/**
+ * Exercicio 2.
+ *
+ * O guia conhece apenas essas 4 regiões, mas tem gente pedindo sugestoes
+ * de outras regioes. Caso alguem pergunte outras sugestoes de regioes que
+ * o guia nao conhece, responda "Não sei".
+ */
+
+/** Descomente as linhas abaixo */
+// conseguirMelhorEstadoDaRegiao("Nordeste")
+// conseguirMelhorEstadoDaRegiao("Noroeste")
