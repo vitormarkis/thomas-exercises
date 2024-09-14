a7cdf0f25fefb5c0fddb55f07b699216c269aac9
Vitor Markis <vitormarkis2369@gmail.com>
2024-09-14 20:45:35 -0300

    create remove keys in english

---
+++ exercises/for-of-for-in/01-remover-chaves-em-ingles.js
@@ -0,0 +1,18 @@
+const pessoa = {
+  nome: "Vitor",
+  idade: 22,
+  sexo: "masculino",
+  old_birthdate: new Date("2002-06-10"),
+  old_age: 22,
+}
+
+/**
+ * Esse objeto pessoa, possui propriedade em portugues, e propriedades em ingles
+ * Quando esse objeto foi traduzido, as chaves em ingles foram marcadas com o pre-
+ * fixo "old_".
+ * Itere sobre as propriedades do objeto pessoa e remova as antigas chaves em ingles.
+ */
+
+/**
+ * SOLUCAO:
+ */
