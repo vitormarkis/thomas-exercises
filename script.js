6cb37d0c927313393d6d2523c43fa9e60654b7c2
vitormarkis <vitormarkis2369@gmail.com>
2025-01-27 20:13:11 -0300

    fiz uma parte do js e tentei resolver alguns erros do meu css que ainfa falta algumas coisas importantes para o desafio( por isso fiz o css copy

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const cellElements = document.querySelectorAll(*[data-cell]);
+const cellElements = document.querySelectorAll("[data-cell]");
+
+let IsCircleTurn = false;
+
+const placeMark = (cell, classToAdd) => {
+  cell.classLista.add(classToAdd);
+  // for (const cell of cellElements {
+  // cell.addEventListener("click", handleClick, { once: true });
+};
+
+const handleClick = (e) => {
+  // colocar a marca (X ou O)
+  const cell = e.target;
+  const classToAdd = IsCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+  //checar vitoria
+  //checar empate
+  //Mudar simbolo
+
+  swapTurns();
+};
 
 for (const cell of cellElements) {
-  cell.addEventListener(*click*, handleClick, { once: true})
+  cell.addEventListener("click", handleClick, { once: true });
 }
