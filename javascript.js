4e96ca33f60842bfd0064beff3d9ceab595aacfe
vitormarkis <vitormarkis2369@gmail.com>
2025-02-04 19:09:27 -0300

    hoje so me organizei um pouco melhor e  "aprendi(falta por em pratica)"  algumas formas de simplificar o if e else fora o ternario, e tentei resolver alguma coisa que deu no app ou no site que o console log/ tudo q vai pro console e duplicado

--- javascript.js
+++ javascript.js
@@ -10,9 +10,9 @@ function verificarpessoa(pessoa) {
   );
 
   if (pegarItemNaoPermitido.length > 0) {
-    return "if";
+    return "Não podera entrar";
   } else {
-    return "else";
+    return "Pode entrar";
   }
 }
 
@@ -20,18 +20,29 @@ let verificado1 = verificarpessoa(ederson);
 let verificado2 = verificarpessoa(enaldo);
 let verificado3 = verificarpessoa(ricardo);
 
-// console.log(verificado1);
+console.log(verificado1);
 // console.log(verificado2);
 // console.log(verificado3);
 
 // resumo codigo
 //linhas 3, 4 e 5
 // criei 3 arrays com 2 com os itens proibidos
+
 // criei uma função  que tem como parametro pessoa que e basicamente um array
-// na linha 8 eu criei a funçao da propria função o usando o filter filtrando/ criando um novo array que passou sob as condições
-// as palavras arma e droga(os itens não permitidos), o || ta verificando se tem os itens arma ou droga
+
+// na linha 8 eu criei a funçao da propria função o usando o filter filtrando/
+//  criando um novo array que passou sob as condições
+
+// as palavras arma e droga(os itens não permitidos), o ||
+//  ta verificando se tem os itens arma ou droga
+
 //o if e o els estão dando a resposta da function se tiver itemnp if se nn tiver else
+
 // e a linha 19-21 e so usando a function nos arrays
+
 //* outras coisas
+
 //pegaritemnaopermitido e o array que fica com itensnp ou fica vazio
+
 //  o "> 0" e uma expressão condicional, ele esta verificando se o valor a esquerda e maior do que o 0, ele serve no codigo para verificar se tem itens não permitidos no array pegaritemnãopermitido
+// o que e o === verifica o valor e o tipo dos operandos.
