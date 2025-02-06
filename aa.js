9ad181ce05e181bc64ccdbd7e8e8cbe460b3d5d9
vitormarkis <vitormarkis2369@gmail.com>
2025-02-06 19:16:56 -0300

    ja que não tinha nada para mim fazer vou fazer codigos de simplificações do if e else hoje fiz do ternario e amanhã do or e and, e fiz alterações em escritas do obisidian

--- aa.js
+++ aa.js
@@ -1,3 +1,21 @@
 //usando simplificaçoes do if e else
 
 //exemplo de ternario
+
+// let thomas = ["chave", "dinheiro", "celular"];
+// let vitor = ["dinheiro", "celular", "carteira"];
+// let pai = ["dinheiro", "celular", "carteira"];
+
+// function abrirporta(pessoa) {
+//   const usarchave = pessoa.filter((item) => item === "chave");
+//   return usarchave.length > 0 ? "abriu porta" : "nao consegue abrir a porta";
+// }
+// let resultado = abrirporta(thomas);
+// let resultado2 = abrirporta(vitor);
+// let resultado3 = abrirporta(pai);
+
+// console.log(resultado);
+// console.log(resultado2);
+// console.log(resultado3);
+
+// or e and
