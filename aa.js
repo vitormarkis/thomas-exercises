c8de4cdbe44bb3d755efa9c0924d6d2a981100c5
vitormarkis <vitormarkis2369@gmail.com>
2025-02-07 20:15:00 -0300

    fiz um exemplo de simplifica~çao d o if e else com o or e and porem não usei o or ja que não precisa na ocasião

--- aa.js
+++ aa.js
@@ -19,3 +19,31 @@
 // console.log(resultado3);
 
 // or e and
+
+let thomas = ["chave", "dinheiro", "celular"];
+let vitor = ["dinheiro", "celular", "carteira"];
+let pai = ["dinheiro", "celular", "carteira"];
+
+function abrirporta(pessoa) {
+  const aloguemtemachave = pessoa.includes("chave");
+  const aloguemtemadinheiro = pessoa.includes("dinheiro");
+
+  switch (true) {
+    case aloguemtemachave:
+      return "abriu a porta com a chave";
+
+    case aloguemtemadinheiro:
+      return "tem dinheiro mais dinheiro não abre a porta.";
+
+    default:
+      return "não tem dinheiro";
+  }
+}
+
+let resultado = abrirporta(thomas);
+let resultado2 = abrirporta(vitor);
+let resultado3 = abrirporta(pai);
+
+console.log(resultado);
+console.log(resultado2);
+console.log(resultado3);
