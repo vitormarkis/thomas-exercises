97d9d3f2cd83ac2c91552aff6046094d36e4f3f7
vitormarkis <vitormarkis2369@gmail.com>
2025-02-10 19:28:16 -0300

    não fiz muita coisa hj apenas adaptei o codigo pra usar o ?? e fiz umas anotações

--- aa.js
+++ aa.js
@@ -18,15 +18,45 @@
 // console.log(resultado2);
 // console.log(resultado3);
 
-// or e and
+// or e and e "switch"
+
+// let thomas = ["chave", "dinheiro", "celular"];
+// let vitor = ["dinheiro", "celular", "carteira"];
+// let pai = ["dinheiro", "celular", "carteira"];
+
+// function abrirporta(pessoa) {
+//   const aloguemtemachave = pessoa.includes("chave");
+//   const aloguemtemadinheiro = pessoa.includes("dinheiro");
+
+//   switch (true) {
+//     case aloguemtemachave:
+//       return "abriu a porta com a chave";
+
+//     case aloguemtemadinheiro:
+//       return "tem dinheiro mais dinheiro não abre a porta.";
+
+//     default:
+//       return "não tem dinheiro";
+//   }
+// }
+
+// let resultado = abrirporta(thomas);
+// let resultado2 = abrirporta(vitor);
+// let resultado3 = abrirporta(pai);
+
+// console.log(resultado);
+// console.log(resultado2);
+// console.log(resultado3);
+
+// SWITCH OR E AND E ??
 
 let thomas = ["chave", "dinheiro", "celular"];
 let vitor = ["dinheiro", "celular", "carteira"];
 let pai = ["dinheiro", "celular", "carteira"];
 
 function abrirporta(pessoa) {
-  const aloguemtemachave = pessoa.includes("chave");
-  const aloguemtemadinheiro = pessoa.includes("dinheiro");
+  const aloguemtemachave = pessoa.includes("chave") ?? false;
+  const aloguemtemadinheiro = pessoa.includes("dinheiro") ?? false;
 
   switch (true) {
     case aloguemtemachave:
